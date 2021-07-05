b91628c906eafddd8918e36212cdd38d86fb666b
MoogleAndChocobo <1728708466@qq.com>
2021-07-05 20:46:05 +0800

    2021.07.05 exercises

--- python/file_exercise/file_operation_exercise1.py
+++ python/file_exercise/file_operation_exercise1.py
@@ -1,1 +1,18 @@
+#!/usr/bin/dev python3
+# -*- coding: utf-8 -*-
 
+__author__ = 'MoogleAndChocobo'
+
+import os
+
+filespath = []
+for x in os.listdir('.'):
+    abspath = os.path.abspath('.')
+    filespath.append(os.path.join(abspath, x))
+print(filespath)
+
+filesinfo = []
+for x in filespath:
+    filesinfo.append(os.stat(x))
+
+print("totol " + len(filesinfo))
